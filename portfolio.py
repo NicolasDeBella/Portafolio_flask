da842c80fb753fed50d37d7572371118059784fc
NicolasDeBella <nicolasfdebella@gmail.com>
2024-03-14 18:49:04 -0300

    subiendo portafolio

---
+++ portfolio.py
@@ -0,0 +1,17 @@
+from flask import (
+    Blueprint,
+    render_template,
+)
+
+bp = Blueprint("portfolio", __name__, url_prefix="/")
+
+@bp.route("/",methods=["GET"])
+def index():
+    return render_template("portfolio/index.html")
+
+@bp.route("/mail",methods=["GET"])
+def mail(): 
+    return render_template("portfolio/sent_mail.html")
+
+
+
